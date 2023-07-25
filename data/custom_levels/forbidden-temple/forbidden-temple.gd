e66d27e1d3e435ffff41ea9d24fb897ade6eea98
KennyThrug <kennyepstein6@gmail.com>
2023-07-25 18:32:47 -0500

    changing textures
    Not complete, but commiting so I can remove all the other changes that I
    want to remove lmao

--- data/custom_levels/forbidden-temple/forbidden-temple.gd
+++ data/custom_levels/forbidden-temple/forbidden-temple.gd
@@ -1,6 +1,12 @@
 
 ("FTP.DGO"
 ("static-screen.o" "forbidden-temple.go")
+("tpage-1513.go" "tpage-1513")
+("tpage-214.go" "tpage-213")
+("tpage-213.go" "tpage-214")
+("tpage-215.go" "tpage-212")
 ("forbidden-temple.go" "forbidden_temple")
 ("plat-ag.go" "plat")
+("bird-lady-ag.go" "bird-lady")
+("gambler-ag.go" "gambler")
 )
